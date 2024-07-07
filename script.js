840e94b85cc8974c5c22d91eacb4195979696ca6
Mike M. <millerdev17@gmail.com>
2024-07-07 01:00:59 -0400

    Reworked layout

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const addBookButton = document.querySelector("[data-open-modal]");
+const addBookButton2 = document.querySelector("[data-open-modal2]");
+const modal = document.querySelector("[data-modal]")
+const closeModal = document.querySelector("[data-close-modal]")
+
 function Book(title, author, pages, haveRead){
     this.title = title;
     this.author = author;
@@ -5,8 +10,24 @@ function Book(title, author, pages, haveRead){
     this.haveRead = haveRead;
 }
 
+addBookButton.addEventListener('click', ()=>{
+    modal.showModal();
+})
+
+addBookButton2.addEventListener('click', ()=>{
+    modal.showModal();
+})
+
+closeModal.addEventListener('click', ()=>{
+    modal.close();
+})
+
+
 
 function toggleRead(){
     
 }
 
+
+// if have read  = no, have toggle button
+//if have read = yes, don't include toggle button
